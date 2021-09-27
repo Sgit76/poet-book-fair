c55acbf01d263994c36b5a79ab7ba03dd4e3d756
Sgit76 <shafiul.cse76@gmail.com>
2021-09-27 19:58:59 +0600

    final update

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './Cart.css'
 const Cart = (props) => {
+    console.log(props);
     const {cart} = props;
     let total = 0;
     for(const writerinfo of cart){
         total = total + writerinfo.price;
     }
-
+    
     return (
         <div className="cart-container">
             <h2>Details</h2>
             <h5>Total Booked: {cart.length}</h5>
             <p>Total Price: {total}</p>
-            <p>Book Name: {props.book}</p>
+            <p>Book Name: {cart[0]?.book}</p>
         </div>
     );
 };
