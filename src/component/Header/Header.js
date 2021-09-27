61c8affa7bc4a74911e11081f6a702150099a12d
Sgit76 <shafiul.cse76@gmail.com>
2021-09-27 11:26:10 +0600

    Header

---
+++ src/component/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <div className="header-element">
+            <h2>Rare books collection fair - 2021</h2>
+            <p>
+                Rare books collection fire in Dhaka ,<br/>
+                <b>Date: </b>2-Oct-2021 to 4-oct-2021.<br/>
+                <b>Place:</b> Public Library , Shahabag , Dhaka.
+            </p>
+            <hr/>
+            <h4>Total collection of books:</h4>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/component/Writers/Writers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Writers = () => {
+    return (
+        <div>
+            <h1>This is writer</h1>
+        </div>
+    );
+};
+
+export default Writers;
